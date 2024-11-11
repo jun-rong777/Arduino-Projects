cd1a6f402e2f1a7f9603dc0fde2ecbd1abf6d7d5
jun-rong777 <ongjunrong41@gmail.com>
2024-11-11 11:40:55 +0800

    Update Internship Project.cpp

--- Internship Project.cpp
+++ Internship Project.cpp
@@ -24,7 +24,8 @@ int holeDistanceThreshold = 10; // Distance threshold for detecting a hole
 int xMax = 0, yMax = 0; // Store detected size (heat exchanger boundaries)
 int startEdge = 0, endEdge = 0; // Store tube edge positions
 
-void setup() {
+void setup() 
+{
   // Set motor speed and acceleration
   motorX.setMaxSpeed(1000); 
   motorX.setAcceleration(500);
@@ -47,10 +48,13 @@ void setup() {
   detectSize();
 }
 
-void loop() {
+void loop() 
+{
   // Dynamic grid scanning to clean each hole
-  for (int yPos = 0; yPos < yMax; yPos += 100) { // Adjust '100' for grid step size
-    for (int xPos = 0; xPos < xMax; xPos += 100) {
+  for (int yPos = 0; yPos < yMax; yPos += 100) 
+  { // Adjust '100' for grid step size
+    for (int xPos = 0; xPos < xMax; xPos += 100)
+      {
       motorX.moveTo(xPos);
       motorY.moveTo(yPos);
       
@@ -74,10 +78,12 @@ void loop() {
 }
 
 // Function to detect the boundaries of the heat exchanger
-void detectSize() {
+void detectSize() 
+{
   // Move X axis until limit switch is triggered
   motorX.move(10000); // Arbitrary large move to find the limit
-  while (digitalRead(limitSwitchX) == HIGH) {
+  while (digitalRead(limitSwitchX) == HIGH) 
+  {
     motorX.run();
   }
   xMax = motorX.currentPosition(); // Store max X position
@@ -85,7 +91,8 @@ void detectSize() {
   
   // Move Y axis until limit switch is triggered
   motorY.move(10000); 
-  while (digitalRead(limitSwitchY) == HIGH) {
+  while (digitalRead(limitSwitchY) == HIGH) 
+  {
     motorY.run();
   }
   yMax = motorY.currentPosition(); // Store max Y position
@@ -99,7 +106,8 @@ void detectSize() {
 }
 
 // Function to detect a hole using the ultrasonic sensor
-bool detectHole() {
+bool detectHole() 
+{
   // Send ultrasonic pulse
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -116,26 +124,30 @@ bool detectHole() {
 }
 
 // Function to detect tube size using ultrasonic sensor
-int detectTubeSize() {
+int detectTubeSize() 
+{
   int tubeDiameter = 0;
   bool edgeDetected = false;
 
   // Start scanning for tube edges
   motorX.moveTo(0); // Start at the center of a hole
-  for (int i = 0; i < 200; i++) {  // Adjust '200' based on expected tube size range
+  for (int i = 0; i < 200; i++) 
+  {  // Adjust '200' based on expected tube size range
     motorX.moveTo(i);
     motorX.run();
     
     int currentDistance = measureDistance();
     
-    if (!edgeDetected && currentDistance < holeDistanceThreshold) {
+    if (!edgeDetected && currentDistance < holeDistanceThreshold) 
+    {
       // Tube edge detected (closer distance)
       startEdge = i;
       edgeDetected = true;
       Serial.println("Start of tube detected.");
     }
     
-    if (edgeDetected && currentDistance >= holeDistanceThreshold) {
+    if (edgeDetected && currentDistance >= holeDistanceThreshold) 
+    {
       // Tube hole detected (farther distance), marking the end of the tube
       endEdge = i;
       Serial.println("End of tube detected.");
@@ -144,7 +156,8 @@ int detectTubeSize() {
   }
   
   // Calculate tube diameter based on start and end positions
-  if (startEdge > 0 && endEdge > 0) {
+  if (startEdge > 0 && endEdge > 0) 
+  {
     tubeDiameter = endEdge - startEdge; // Diameter in step units
   }
   
@@ -152,7 +165,8 @@ int detectTubeSize() {
 }
 
 // Function to measure distance using ultrasonic sensor
-int measureDistance() {
+int measureDistance() 
+{
   // Send ultrasonic pulse
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
